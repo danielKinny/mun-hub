9479760396bdfc04af620aab32ac4c0d09f2cd04
danielKinny <danielkinny0214@gmail.com>
2025-05-23 22:14:48 +0400

    scaffolding the layout

--- app/speechrepo/page.tsx
+++ app/speechrepo/page.tsx
@@ -9,13 +9,40 @@ import { toast } from "sonner";
 
 const Page = () => {
   const { user: currentUser } = useSession();
+  const [speech, setSpeech] = React.useState<Speech>({
+    speechID: "",
+    title: "",
+    content: "",
+    tags: [],
+  });
+  const [speechList, setSpeechList] = React.useState<Speech[]>([]);
+  const [heading, setHeading] = React.useState<string>("");
+  const [content, setContent] = React.useState<string>("");
+
   return (
     <ProtectedRoute>
       <CustomNav />
-      <div className="text-white">
-        <div>
+      <div className="flex text-white h-screen p-4 bg-gradient-to-b from-black to-gray-950">
+        <div className="bg-gray-900 w-1/4 h-full rounded-2xl p-4">
           this will be were speeches are listed
         </div>
+        <div className="w-full space-y-2">
+          <textarea
+          className="block w-full outline rounded-2xl p-4 ml-4"
+          placeholder="Write your speech here..."
+          onChange={(e) => { setHeading(e.target.value); }
+          }
+          value={heading}
+        ></textarea>
+        <textarea
+          className="outline w-full h-full rounded-2xl p-4 ml-4"
+          placeholder="Write your speech here..."
+          onChange={(e) => { setContent(e.target.value); }
+          }
+          value={content}
+        ></textarea>
+        </div>
+        
       </div>
     </ProtectedRoute>
   );
