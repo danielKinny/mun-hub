9f60a490133698b77d6764d654b7fca5ba5683df
danielKinny <danielkinny0214@gmail.com>
2025-06-01 18:09:44 +0400

    basic news api implemented

--- app/global-affairs/page.tsx
+++ app/global-affairs/page.tsx
@@ -1,10 +1,69 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import ProtectedRoute from "@/components/protectedroute";
+import { CustomNav } from "@/components/ui/customnav";
+import Image from "next/image";
+
+interface Article {
+
+  source: {
+    id: string;
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+}
 
 export default function Page() {
+  const [selectedArticle, setSelectedArticle] = useState<Article | null>(null);
+
+  const [articles, setArticles] = useState<Article[] | null>(null);
+
+  useEffect(() => {
+    const fetchSpeeches = async () =>
+    {const res = await fetch("/api/articles");
+    if (!res.ok) {
+      console.error("Failed to fetch articles");
+      return;
+    }
+    const data = await res.json();
+    setArticles(data);}
+
+    fetchSpeeches();
+  }, []);
+
   return (
     <ProtectedRoute>
-      <div></div>
+      <CustomNav />
+      <main className="min-h-screen text-white">
+        <section className="columns-1 sm:columns-2 md:columns-3 lg:columns-5 gap-4 p-4 [column-fill:_balance]">
+          {articles?.map((article) => (
+            <div
+              key={article.source.id + article.publishedAt}
+              className="mb-4 break-inside-avoid p-4 bg-gray-700 rounded-lg cursor-pointer hover:bg-gray-600 w-full inline-block"
+              onClick={() => setSelectedArticle(article)}
+            >
+              <h2 className="text-xl font-bold">{article.title}</h2>
+              <img
+                src={article.urlToImage}
+                width={200}
+                height={200}
+                alt="news cover"
+                className="object-fit my-2 w-full h-auto rounded"
+              />
+              <p className="text-sm text-gray-400">
+                {new Date(article.publishedAt).toLocaleDateString()}
+              </p>
+              <p className="mt-2">{article.description}</p>
+            </div>
+          ))}
+        </section>
+      </main>
     </ProtectedRoute>
   );
 }
